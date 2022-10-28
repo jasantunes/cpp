a4addc66098bece8192416dae12702cfa27f64e5
Joao Antunes <jasantunes@gmail.com>
2022-10-28 11:42:02 -0700

    How to convert std::unique_ptr<PluginTester<T>> to std::unique_ptr<PluginTester<AppPluginWithClient>> ?

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -25,24 +25,26 @@ struct PluginTester : public T {
 
 struct ParticipantPipelineTester {
   template <typename T>
-  std::shared_ptr<PluginTester<T>> makeAndConnectPlugin() {
-    return std::make_shared<PluginTester<T>>();
+  std::unique_ptr<PluginTester<T>> makeAndConnectPlugin() {
+    return std::make_unique<PluginTester<T>>();
   }
 };
 
 struct BaseApperPluginTest {
-  std::vector<std::shared_ptr<void>> plugins_;
+  std::vector<std::unique_ptr<PluginTester<AppPluginWithClient>>> plugins_;
 
   template <typename T>
-  void makeAndConnectPlugin(const std::shared_ptr<ParticipantPipelineTester>& pp) {
+  void makeAndConnectPlugin(const std::unique_ptr<ParticipantPipelineTester>& pp) {
     auto plugin = pp->makeAndConnectPlugin<T>();
-    plugins_.push_back(plugin);
+    // plugin is type: std::unique_ptr<PluginTester<T>>
+    // T is subclass of AppPluginWithClient
+    // How to convert std::unique_ptr<PluginTester<T>> -> std::unique_ptr<PluginTester<AppPluginWithClient>>?
+    auto* ptr1 = reinterpret_cast<PluginTester<AppPluginWithClient>*>(plugin.release());
+    plugins_.push_back(std::unique_ptr<PluginTester<AppPluginWithClient>>(ptr1));
   }
 
   void shutdown() {
-    for (const auto& ptr : plugins_) {
-      auto plugin =
-          std::static_pointer_cast<PluginTester<AppPluginWithClient>>(ptr);
+    for (const auto& plugin : plugins_) {
       plugin->co_shutdown();
     }
     plugins_.clear();
@@ -51,12 +53,11 @@ struct BaseApperPluginTest {
 
 int main() {
   auto test = BaseApperPluginTest();
-  auto pp = std::make_shared<ParticipantPipelineTester>();
+  auto pp = std::make_unique<ParticipantPipelineTester>();
   test.makeAndConnectPlugin<Plugin1>(pp);
   test.makeAndConnectPlugin<Plugin2>(pp);
   std::cout << "size: " << test.plugins_.size() << std::endl;
   test.shutdown();
   std::cout << "size: " << test.plugins_.size() << std::endl;
-
 }
 
