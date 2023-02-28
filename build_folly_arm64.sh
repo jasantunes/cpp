705ba9c344a0ed76ae9b2ebe5ded9101d490ac03
Joao Antunes <jasantunes@gmail.com>
2023-02-27 21:23:53 -0800

    Fix CMAKE option used in folly's build script

--- build_folly_arm64.sh
+++ build_folly_arm64.sh
@@ -7,11 +7,11 @@ SCRATCH_PATH=${SCRATCH_PATH:-${PWD}/deps}
 
 # Clone the repo
 mkdir -p deps
-[[ -d deps/folly ]] || git clone https://github.com/facebook/folly deps/folly
+[ -d deps/folly ] || git clone https://github.com/facebook/folly deps/folly
 cd deps/folly
 
 # Build, using system dependencies if available, and some custom flags for arm64
-python3 ./build/fbcode_builder/getdeps.py --allow-system-packages --scratch-path "${SCRATCH_PATH}" build --extra-cmake-defines='{"DCMAKE_LIBRARY_ARCHITECTURE":"arm64", "CMAKE_CXX_FLAGS":"-fcoroutines-ts"}'
+python3 ./build/fbcode_builder/getdeps.py --allow-system-packages --scratch-path "${SCRATCH_PATH}" build --extra-cmake-defines='{"CMAKE_LIBRARY_ARCHITECTURE":"arm64", "CMAKE_CXX_FLAGS":"-fcoroutines-ts"}'
 cd ..
 
 echo """
