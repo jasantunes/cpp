235a955cc078c6f009c1b217704bc14f403e7a52
Joao Antunes <jasantunes@gmail.com>
2023-04-14 16:22:51 -0700

    Remove redundant example file

--- src/05_folly_coroutines.cpp
+++ src/05_folly_coroutines.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 
+#include <folly/experimental/coro/AsyncScope.h>
 #include <folly/experimental/coro/Baton.h>
 #include <folly/experimental/coro/BlockingWait.h>
 #include <folly/experimental/coro/Collect.h>
