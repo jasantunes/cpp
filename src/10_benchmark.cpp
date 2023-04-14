3706aed034474c23e0db43319f7d4fbd10272cc7
Joao Antunes <jasantunes@gmail.com>
2023-04-14 15:25:17 -0700

    Add first functional example (accumulate) and update benchmark

--- src/10_benchmark.cpp
+++ src/10_benchmark.cpp
@@ -1,25 +1,51 @@
 #include <folly/Benchmark.h>
 #include <vector>
+#include <numeric>
+
 
 using namespace std;
 using namespace folly;
 
-BENCHMARK(insertFrontVector) {
-  // Let's insert 100 elements at the front of a vector
-  vector<int> v;
-  for (unsigned int i = 0; i < 100; ++i) {
-    v.insert(v.begin(), i);
+// auto vec = std::vector<std::string>{{"key1", "key2", "key3", "key4", "key5"}};
+auto comma = [](std::string a, std::string b) {
+    return (a.empty()) ? std::move(b) : std::move(a) + ", " + std::move(b);
+};
+
+
+BENCHMARK(accumulate, n) {
+  folly::BenchmarkSuspender suspender;
+  std::vector<std::string> vec{};
+  for (auto i = 0; i < n; ++i) {
+    vec.emplace_back("key" + std::to_string(i));
   }
+  suspender.dismiss(); // start measuring benchmark time
+
+  std::accumulate(vec.begin(), vec.end(), std::string{}, comma);
+
+  folly::doNotOptimizeAway(vec);
+
+  suspender.rehire(); // stop measuring benchmark time
+  // vec is destructed at this point
+  // suspender is destructed last because it is declared first
 }
 
-BENCHMARK(insertBackVector) {
-  // Let's insert 100 elements at the back of a vector
-  vector<int> v;
-  for (unsigned int i = 0; i < 100; ++i) {
-    v.insert(v.end(), i);
+BENCHMARK(reduce, n) {
+  folly::BenchmarkSuspender suspender;
+  std::vector<std::string> vec{};
+  for (auto i = 0; i < n; ++i) {
+    vec.emplace_back("key" + std::to_string(i));
   }
+  suspender.dismiss();
+
+  std::reduce(vec.begin(), vec.end(), std::string{}, comma);
+
+  folly::doNotOptimizeAway(vec);
+
+  suspender.rehire();
 }
 
-int main() {
+// run with --bm_min_iters=100000
+int main(int argc, char** argv) {
+  // facebook::initFacebook(&argc, &argv);
   runBenchmarks();
 }
---
+++ src/15_functional.cpp
@@ -0,0 +1,44 @@
+#include <memory>
+#include <vector>
+#include <iostream>
+#include <array>
+#include <optional>
+#include <range/v3/all.hpp>
+// #include <range/v3/view/join.hpp>
+#include <algorithm> // copy
+
+
+bool foo(std::string& str) {
+  str.assign("new value");
+  return true;
+}
+auto comma = [](std::string a, std::string b) {
+    return (a.empty()) ? std::move(b) : std::move(a) + ", " + std::move(b);
+};
+int main() {
+  std::string str{};
+  std::cout << "str: " << str << std::endl;
+  if (foo(str)) {
+    std::cout << "modified to: " << str << std::endl;
+  }
+
+  auto vec = std::vector<std::string>{{"key1", "key2"}};
+
+  {
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<std::string>(std::cout, ", "));
+    std::cout << std::endl;
+  }
+
+  {
+    std::string s = std::accumulate(vec.begin(), vec.end(), std::string{}, comma);
+    std::cout << "vec: " << s << std::endl;
+  }
+
+  {
+    std::string s = std::reduce(vec.begin(), vec.end(), std::string{}, comma);
+    std::cout << "vec: " << s << std::endl;
+  }
+
+
+}
+
